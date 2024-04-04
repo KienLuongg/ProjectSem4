ad95482aa08dc1f3429990467df790beb17201d5
user <mrkiencute@gmail.com>
2024-04-04 12:02:16 +0700

    2nd commit

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import { Suspense, lazy, useEffect, useState } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
+import ECommerce from './pages/Dashboard/ECommerce';
+import SignIn from './pages/Authentication/SignIn';
+import SignUp from './pages/Authentication/SignUp';
+import Loader from './common/Loader';
+import routes from './routes';
+
+const DefaultLayout = lazy(() => import('./layout/DefaultLayout'));
+
+function App() {
+  const isLoggedIn = localStorage.getItem('loginSuccessMessage');
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        containerClassName="overflow-auto"
+      />
+      <Routes>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        {isLoggedIn ? (
+          <Route element={<DefaultLayout />}>
+            <Route index element={<ECommerce />} />
+            {routes.map((route, index) => {
+              const { path, component: Component } = route;
+              return (
+                <Route
+                  key={index}
+                  path={path}
+                  element={
+                    <Suspense fallback={<Loader />}>
+                      <Component />
+                    </Suspense>
+                  }
+                />
+              );
+            })}
+          </Route>
+        ) : (
+          <Route path="/sign-in" element={<SignIn />} />
+        )}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/index.ts
@@ -0,0 +1,39 @@
+import { lazy } from 'react';
+
+const Students = lazy(() => import('../pages/Students/Student-list'));
+const SchoolYears = lazy(() => import('../pages/School-years/School-years'));
+const ClassesList = lazy(() => import('../pages/Classes/Classes-list'));
+const Teachers = lazy(() => import('../pages/Teachers/Teachers'))
+const NotFound = lazy(() => import('../pages/404page'));
+
+
+const coreRoutes = [
+  {
+    path: '*',
+    title: 'NotFound',
+    component: NotFound,
+  },
+  {
+    path: '/students',
+    title: 'Students',
+    component: Students,
+  },
+  {
+    path: '/school-years',
+    title: 'School Years',
+    component: SchoolYears,
+  },
+  {
+    path: '/classes',
+    title: 'Classes',
+    component: ClassesList,
+  },
+  {
+    path: '/teachers',
+    title: 'Teachers',
+    component: Teachers,
+  },
+];
+
+const routes = [...coreRoutes];
+export default routes;
