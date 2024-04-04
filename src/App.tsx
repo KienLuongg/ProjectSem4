2fe27e607d67f5302899703f6281073f32b7339f
user <mrkiencute@gmail.com>
2024-04-04 21:42:27 +0700

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,52 @@
-import { Suspense, lazy, useEffect, useState } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Fragment, Suspense, lazy, useEffect, useState } from 'react';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-
-import ECommerce from './pages/Dashboard/ECommerce';
-import SignIn from './pages/Authentication/SignIn';
-import SignUp from './pages/Authentication/SignUp';
 import Loader from './common/Loader';
 import routes from './routes';
+import { getCookie } from './utils/storage/cookie-storage';
+import { Storage } from './contstants/storage';
 
 const DefaultLayout = lazy(() => import('./layout/DefaultLayout'));
+const NotFound = lazy(() => import('./pages/404page'));
+const SignIn = lazy(() => import('./pages/Authentication/SignIn'));
+const SignUp = lazy(() => import('./pages/Authentication/SignUp'));
+const ECommerce = lazy(() => import('./pages/Dashboard/ECommerce'));
 
 function App() {
-  const isLoggedIn = localStorage.getItem('loginSuccessMessage');
-  const [loading, setLoading] = useState<boolean>(true);
-
+  const [loading, setLoading] = useState(true);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const token = getCookie(Storage.token);
+  console.log(token);
   useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
-  }, []);
+    setLoading(false);
+    if (!token) {
+      navigate('/sign-in');
+    }
+  }, [token, navigate]);
 
-  return loading ? (
-    <Loader />
-  ) : (
-    <>
-      <Toaster
-        position="top-right"
-        reverseOrder={false}
-        containerClassName="overflow-auto"
-      />
-      <Routes>
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        {isLoggedIn ? (
-          <Route element={<DefaultLayout />}>
-            <Route index element={<ECommerce />} />
-            {routes.map((route, index) => {
-              const { path, component: Component } = route;
-              return (
-                <Route
-                  key={index}
-                  path={path}
-                  element={
-                    <Suspense fallback={<Loader />}>
-                      <Component />
-                    </Suspense>
-                  }
-                />
-              );
-            })}
-          </Route>
-        ) : (
+  return (
+    <Suspense fallback={<Loader />}>
+      <Fragment>
+        <Toaster position="top-right" reverseOrder={false} />
+        <Routes>
+          <Route path="*" element={<NotFound />} />
           <Route path="/sign-in" element={<SignIn />} />
-        )}
-      </Routes>
-    </>
+          {token ? (
+            <Route path="/" element={<DefaultLayout />}>
+              <Route index element={<ECommerce />} />
+              {routes.map((route, index) => {
+                const { path, component: Component } = route;
+                return (
+                  <Route key={index} path={path} element={<Component />} />
+                );
+              })}
+            </Route>
+          ) : null}
+          {/* <Route path="/sign-up" element={<SignUp />} /> */}
+        </Routes>
+      </Fragment>
+    </Suspense>
   );
 }
 
